33b0523ea55417f44f7f367c69d4dcdac0a24c60
69070078 <69070078@kmitl.ac.th>
2026-07-14 00:49:34 +0700

    add:code3011

---
+++ 3011/[LEARNING LOGS] Colors.py
@@ -0,0 +1,20 @@
+"""Colors"""
+def main():
+    """Colors"""
+    color4 = input().strip()
+    color5 = input().strip()
+    if (color4 == "Red" and color5 == "Blue") or (color5 == "Red" and color4 == "Blue"):
+        print("Violet")
+    elif (color4 == "Red" and color5 == "Yellow") or (color5 == "Red" and color4 == "Yellow"):
+        print("Orange")
+    elif (color4 == "Yellow" and color5 == "Blue") or (color5 == "Yellow" and color4 == "Blue"):
+        print("Green")
+    elif color4 == "Red" and color5 == "Red":
+        print("Red")
+    elif color4 == "Blue" and color5 == "Blue":
+        print("Blue")
+    elif color4 == "Yellow" and color5 == "Yellow":
+        print("Yellow")
+    else:
+        print("Error")
+main()
